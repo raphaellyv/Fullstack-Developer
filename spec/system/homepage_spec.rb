608d22c6a5f8f3fd5464e4c8b288145832c17f0f
Raphaelly Valiengo <102007947+raphaellyv@users.noreply.github.com>
2025-11-02 19:35:25 -0300

    Add home link to navbar

--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -4,7 +4,7 @@ describe 'Homepage', type: :system do
   it 'shows app name and welcome message' do
     visit(root_path)
 
-    expect(page).to have_content('Users Management App')
+    expect(page).to have_link('Users Management App')
     expect(page).to have_content('Welcome, folks!')
   end
 end
