67a74ffa1dd0726ac6793972c02bd88add3a2df9
Raphaelly Valiengo <102007947+raphaellyv@users.noreply.github.com>
2025-11-02 03:06:20 -0300

    Add Simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'simplecov'
+SimpleCov.start
+
 require 'support/factory_bot'
 require 'support/shoulda_matchers'
 
