f11e7bc9ce630f29f96d99c94370fc7367ec8d72
Raphaelly Valiengo <102007947+raphaellyv@users.noreply.github.com>
2025-11-03 03:26:56 -0300

    Add missing registration fields

--- spec/system/user/sign_up_spec.rb
+++ spec/system/user/sign_up_spec.rb
@@ -2,24 +2,26 @@ require "rails_helper"
 
 describe 'User Sign Up', type: :system do
   context 'success' do
-    xit 'shows user first name and sign out option' do
-      user = create(:user)
-
+    it 'shows user first name and sign out option' do
       visit(root_path)
       click_on('Sign In')
+      click_on('Sign up')
 
       within 'form' do
-        fill_in 'Email', with: user.email
-        fill_in 'Password', with: user.password
-        click_on('Sign in')
+        fill_in 'Full name', with: 'João da Silva'
+        fill_in 'Email', with: 'jsilva@email.com'
+        fill_in 'Password', with: '123456'
+        fill_in 'Password confirmation', with: '123456'
+        page.attach_file('Avatar image', Rails.root.join('spec', 'fixtures', 'files', 'avatar_image.jpg'))
+        click_on('Sign up')
       end
 
       expect(page).not_to have_link('Sign In')
       expect(page).to have_link('Profile')
       expect(page).to have_button('Sign Out')
-      expect(page).to have_content("Signed in successfully.")
-      expect(page).to have_content(user.full_name)
-      expect(page).to have_content(user.email)
+      expect(page).to have_content('Welcome! You have signed up successfully.')
+      expect(page).to have_content('João da Silva')
+      expect(page).to have_content('jsilva@email.com')
     end
   end
 
